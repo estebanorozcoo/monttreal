a63fece1dc640616c59c470113383ac9d348b8dc
estebanorozcoo <estebanorozcosoorio@gmail.com>
2025-12-04 13:40:45 -0500

    Ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: 'Monttreal',
+  title: 'Monttreal | Exclusivas Prendas',
   description: '| Prendas exclusivas',
 };
 
